3289b48076be11ff2a2c00e9fc3a2093e5afa810
Nileshlejhro <nilesh.pushkarna@lejhro.com>
2024-02-21 08:27:48 +0530

    Changes in Destination

--- src/Components/fakeData/fakeData.js
+++ src/Components/fakeData/fakeData.js
@@ -41,27 +41,27 @@ export const stay = [
 
 
 // From locations
-export const From = ['Sylhet', 'Dhaka', 'Chittagong', 'Rangpur', 'Barisal', 'Khulna', 'Rajshahi', 'Mymensingh'];
+export const From = ['Udaipur', 'Chittorgarh', 'Jaipur', 'Jodhpur', 'Ajmer', 'Rajsamand', 'Bikaner'];
 
 
 // Fake Data of Destinatioin
 export const destinations = [
     {
         id: 0,
-        name: 'Sreemongle',
-        description: "Sreemangal is a charming town in Bangladesh, where visitors can immerse themselves in nature and explore the area's many natural wonders. The town is surrounded by lush green tea gardens, and visitors can sample the delicious local brews and learn about the tea-making process. Don't miss a visit to the Lawachara National Park, Madhabpur Lake, and Baikka Beel Wetland Sanctuary for a truly unforgettable experience.",
-        img: 'Sreemongol'
+        name: 'Jagdish Temple',
+        description: "Jagdish Temple is a charming town in India, where visitors can immerse themselves in nature and explore the area's many natural wonders. The town is surrounded by lush green tea gardens, and visitors can sample the delicious local brews and learn about the tea-making process. Don't miss a visit to the Saheliyon ki bari,City Palace ,Shilpgram",
+        img: 'Jagdish Temple'
     },
-    {
-        id: 1,
-        name: 'Sundorbans',
-        description: "Discover the stunning Sundarbans, a natural treasure trove and the world's largest mangrove forest. Get ready for an affordable adventure where you can witness majestic Bengal tigers and saltwater crocodiles in their natural habitat. Take a serene boat ride through the forest and its tributaries, or immerse yourself in the local fishing villages to learn about the unique way of life here. With its lush greenery, breathtaking beauty, and affordable prices, Sundarbans is a perfect destination for budget-conscious travelers seeking an unforgettable experience. Come and explore this hidden gem and create memories that will last a lifetime!",
-        img: 'sundorbon'
-    },
-    {
-        id: 2,
-        name: 'Sajek',
-        description: "Sajek Valley is a beautiful place in Bangladesh, located in the Chittagong Hill Tracts. It has natural beauty and a cool climate that is perfect for a peaceful getaway. The valley is surrounded by hills and forests and offers many activities like hiking, camping, and enjoying the views of sunrise and sunset. The local indigenous people are very friendly and welcoming, and visitors can learn about their unique culture. If you want a memorable experience, make sure to visit Sajek Valley.",
-        img: 'Sajek'
-    },
+    // {
+    //     id: 1,
+    //     name: 'Sundorbans',
+    //     description: "Discover the stunning Sundarbans, a natural treasure trove and the world's largest mangrove forest. Get ready for an affordable adventure where you can witness majestic Bengal tigers and saltwater crocodiles in their natural habitat. Take a serene boat ride through the forest and its tributaries, or immerse yourself in the local fishing villages to learn about the unique way of life here. With its lush greenery, breathtaking beauty, and affordable prices, Sundarbans is a perfect destination for budget-conscious travelers seeking an unforgettable experience. Come and explore this hidden gem and create memories that will last a lifetime!",
+    //     img: 'sundorbon'
+    // },
+    // {
+    //     id: 2,
+    //     name: 'Sajek',
+    //     description: "Sajek Valley is a beautiful place in Bangladesh, located in the Chittagong Hill Tracts. It has natural beauty and a cool climate that is perfect for a peaceful getaway. The valley is surrounded by hills and forests and offers many activities like hiking, camping, and enjoying the views of sunrise and sunset. The local indigenous people are very friendly and welcoming, and visitors can learn about their unique culture. If you want a memorable experience, make sure to visit Sajek Valley.",
+    //     img: 'Sajek'
+    // },
 ];
