525e2f1afb018df7d116ee77e698a2259a56249c
Nileshlejhro <nilesh.pushkarna@lejhro.com>
2024-02-21 08:31:00 +0530

    changes in some destinations files

--- src/Components/fakeData/fakeData.js
+++ src/Components/fakeData/fakeData.js
@@ -54,14 +54,14 @@ export const destinations = [
     },
     {
         id: 1,
-        name: 'Sundorbans',
-        description: "Discover the stunning Sundarbans, a natural treasure trove and the world's largest mangrove forest. Get ready for an affordable adventure where you can witness majestic Bengal tigers and saltwater crocodiles in their natural habitat. Take a serene boat ride through the forest and its tributaries, or immerse yourself in the local fishing villages to learn about the unique way of life here. With its lush greenery, breathtaking beauty, and affordable prices, Sundarbans is a perfect destination for budget-conscious travelers seeking an unforgettable experience. Come and explore this hidden gem and create memories that will last a lifetime!",
+        name: 'City Palace',
+        description: "Discover the stunning City Palace, a natural treasure trove and the world's largest mangrove forest. Get ready for an affordable adventure where you can witness majestic Bengal tigers and saltwater crocodiles in their natural habitat. Take a serene boat ride through the forest and its tributaries, or immerse yourself in the local fishing villages to learn about the unique way of life here. With its lush greenery, breathtaking beauty, and affordable prices, Sundarbans is a perfect destination for budget-conscious travelers seeking an unforgettable experience. Come and explore this hidden gem and create memories that will last a lifetime!",
         img: 'sundorbon'
     },
     {
         id: 2,
-        name: 'Sajek',
-        description: "Sajek Valley is a beautiful place in Bangladesh, located in the Chittagong Hill Tracts. It has natural beauty and a cool climate that is perfect for a peaceful getaway. The valley is surrounded by hills and forests and offers many activities like hiking, camping, and enjoying the views of sunrise and sunset. The local indigenous people are very friendly and welcoming, and visitors can learn about their unique culture. If you want a memorable experience, make sure to visit Sajek Valley.",
-        img: 'Sajek'
+        name: 'Shilpgram',
+        description: "Shilpgram  is a beautiful place in Udaipur, located in the Chittagong Hill Tracts. It has natural beauty and a cool climate that is perfect for a peaceful getaway. The valley is surrounded by hills and forests and offers many activities like hiking, camping, and enjoying the views of sunrise and sunset. The local indigenous people are very friendly and welcoming, and visitors can learn about their unique culture. If you want a memorable experience, make sure to visit Sajek Valley.",
+        img: 'Shilpgram'
     },
 ];
